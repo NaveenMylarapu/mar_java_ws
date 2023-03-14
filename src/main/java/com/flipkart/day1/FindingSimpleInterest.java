65ad49763dae03ec72590bb20226b646b00428db
naveenmailarapu <naveenkanni17198@gmail.com>
2023-03-14 17:27:03 -0400

    Day 1 Programs

---
+++ src/main/java/com/flipkart/day1/FindingSimpleInterest.java
@@ -0,0 +1,14 @@
+package com.flipkart.day1;
+
+//Write a program to find the simple interest based on the following data: P = Rs 6000, R = 10%, T = 1.5 years.
+public class FindingSimpleInterest {
+    public static void main(String[] args) {
+        int p = 6000;
+        double r = 0.1;
+        double t = 1.5;
+        double simpleinterest = (p * t * r) / 100;
+        System.out.println("The simple Interest for given values is : " + simpleinterest);
+
+    }
+
+}
